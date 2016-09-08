b9ea0b25776ef9717005c065f66c6c82c17adbf5
Alan Ehret <alan.4186@yahoo.com>
2016-09-08 12:05:31 -0400

    finished sub_sample

--- Hardware/network_params.h
+++ Hardware/network_params.h
@@ -1,1 +1,29 @@
+`define NN_WIDTH 16
+`define NN_BITWIDTH `NN_WIDTH - 1
+
+
+// Sub sampling
 `define NEIGHBORHOOD_SIZE 4
+`define NH_VECTOR_WIDTH `NEIGHBORHOOD_SIZE*`NN_WIDTH
+`define NH_VECTOR_BITWIDTH NH_VECTOR_WIDTH - 1 
+`define NUM_NH_LAYERS CLOG(`NEIGHBORHOOD_SIZE)
+`define ADDER_TREE_BITWIDTH `NN_BITWIDTH+`NUM_NH_LAYERS
+
+
+
+// Softmax 
+`define SOFTMAX_IN_VECTOR_LENGTH TBD // the number of inputs to the softmax layer
+`define NUM_CLASSES 10 // number of output classes for the entire nn
+
+// Normalization (for Softmax
+`define NUM_NORM_LAYERS `CLOG(`NUM_CLASSES) ????
+
+// Math macros
+`define CLOG2(x) \
+    (x <= 2) ? 1 : \
+    (x <= 4) ? 2 : \
+    (x <= 8) ? 3 : \
+    (x <= 16) ? 4 : \
+    (x <= 32) ? 5 : \
+    (x <= 64) ? 6 : \
+    INVALID_LOG
--- Hardware/v/sub_sample.v
+++ Hardware/v/sub_sample.v
@@ -1,25 +1,24 @@
 `include <network_parameters.h>
-`define VECTOR_WIDTH `NEIGHBORHOOD_SIZE*`NN_WIDTH
-`define VECTOR_BITWIDTH VECTOR_WIDTH - 1
-`define NUM_LAYERS CLOG(`NEIGHBORHOOD_SIZE)
 module sub_sample( // Mean pooling
   input clock,
   input reset,
-  input [VECTOR_BITWIDTH:0] nh_vector, // ` indicates a difined macro
-  output [`NN_BITWIDTH:0] out
+  input [NH_VECTOR_BITWIDTH:0] nh_vector, // ` indicates a difined macro
+  output [`NN_BITWIDTH:0] pool_out;
 );
 
 // wires
-wire [`NN_BITWIDTH+`NUM_LAYERS:0] adder_tree_wire [`NUM_LAYERS+1][`NEIGHBORHOOD_SIZE];
+wire [`ADDER_TREE_BITWIDTH:0] adder_tree_wire [`NUM_NH_LAYERS+1][`NEIGHBORHOOD_SIZE];
 
 // regs
 
 
+// assign statments
+assign pool_out = adder_tree_wire[`NUM_NH_LAYERS+1][`NEIGHBORHOOD_SIZE/2] / `ADDER_TREE_BITWIDTH'd`NEIGHBORHOOD_SIZE;
 
 genvar i
 genvar j
 generate
-for(i=0; i < `NUM_LAYERS; i=i+1) begin
+for(i=0; i < `NUM_NH_LAYERS; i=i+1) begin
   for(j=0; j < `NEIGHBORHOOD_SIZE - i; j=j+2) begin
     add add_inst ( 
       .add_a(adder_tree_wire[i][j]),
@@ -34,11 +33,9 @@ endmodule
 
 
 module add (
-  input [`NN_BITWIDTH + `NUMLAYERS:0] add_a,
-  input [`NN_BITWIDTH + `NUMLAYERS:0] add_b,
-  output [`NN_BITWIDTH + `NUMLAYERS:0] out
+  input [`ADDER_TREE_BITWIDTH:0] add_a,
+  input [`ADDER_TREE_BITWIDTH:0] add_b,
+  output [`ADDER_TREE_BITWIDTH:0] out
 );
-
 assign out = add_a + add_b;
-
 endmodule
