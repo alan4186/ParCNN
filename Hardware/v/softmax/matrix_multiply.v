4e4823c939acc2bbedafb42781cd2b735bf7a836
Alan Ehret <alan.4186@yahoo.com>
2016-09-17 21:18:02 -0400

    removed basic functions from matrix_mult.v

--- Hardware/v/softmax/matrix_multiply.v
+++ Hardware/v/softmax/matrix_multiply.v
@@ -63,43 +63,5 @@ generate
     end
   end
 endgenerate
-endmodule
-
-module multiply2( // Multiply 2 fixed point numbers
-  input clock,
-  input reset,
-
-  input [`FFN_BITWIDTH:0] operand_a,
-  input [`FFN_BITWIDTH:0] operand_b,
-
-  output [`FFN_OUT_BITWIDTH:0] product
-);
-
-always@(posedge clock or negedge reset) begin
-  if(reset == 1;b0) begin
-    product <= `FFN_OUT_WIDTH'd0;
-  end else begin
-    product <= {`CLOG(`NUM_INPUT_N)'d0, operand_a * operand_b} ;
-  end // reset
-end // always
-
-endmodule
-
-
-module add2(
-  input clock,
-  input reset,
-
-  input [`FFN_OUT_BITWIDTH:0] operand_a,
-  input [`FFN_OUT_BITWIDTH:0] operand_b,
-  output [`FFN_OUT_BITWIDTH:0] sum
-);
-
-always@(posedge clock or negedge reset) begin
-  if(reset == 1'b0) 
-    sum <= `FFN_OUT_WIDTH'd0;
-  else 
-    sum <= operand_a + operand_b;
-end // always
 
 endmodule
