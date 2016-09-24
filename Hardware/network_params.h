41ac073db90f2b8a4c71bbd993045289fe2d4cca
Alan Ehret <alan.4186@yahoo.com>
2016-09-23 20:39:03 -0400

    fixed some bugs, normalize.v synthesizes

--- Hardware/network_params.h
+++ Hardware/network_params.h
@@ -1,67 +1,29 @@
-// General Network Parameters
-`define INPUT_SIZE 28 // dimension of square input image
-`define NUM_KERNELS 10
-`define KERNEL_SIZE 9 // square kernel
-`define FEATURE_SIZE `INPUT_SIZE - `KERNEL_SIZE + 1 // The dimension of the convolved image
-
-// General Bitwidths
+`define NORM_OUT_BITWIDTH 45
+`define FFN_WIDTH 16
+`define ADDER_TREE_BITWIDTH 17
+`define NUM_NORM_LAYERS 2
+`define SOFTMAX_IN_VECTOR_LENGTH 1000
+`define ADDER_TREE_WIDTH 17
+`define FEATURE_SIZE 20
+`define NUM_CLASSES 4
+`define NH_VECTOR_BITWIDTH 63
+`define FFN_BITWIDTH 15
+`define FFN_OUT_WIDTH 44
+`define ADDER_TREE_PAD 2
+`define MEAN_DIVSION_CONSTANT 17'd4
 `define NN_WIDTH 16
-`define NN_BITWIDTH `NN_WIDTH - 1
-
-
-// Rect Linear
-
-
-// Sub sampling
+`define NORM_IN_WIDTH 44
+`define INPUT_SIZE 28
+`define NUM_INPUT_IM 1
+`define NN_BITWIDTH 15
+`define NUM_KERNELS 10
+`define NH_VECTOR_WIDTH 64
+`define NUM_INPUT_N 4000
+`define FFN_OUT_BITWIDTH 43
+`define NUM_NH_LAYERS 2
+`define NORM_OUT_WIDTH 46
 `define NEIGHBORHOOD_SIZE 4
-`define NH_VECTOR_WIDTH `NEIGHBORHOOD_SIZE*`NN_WIDTH
-`define NH_VECTOR_BITWIDTH `NH_VECTOR_WIDTH - 1 
-`define NUM_NH_LAYERS `LOG2(`NEIGHBORHOOD_SIZE)
-//`define NUM_NH_LAYERS PNUM_NH_LAYERS
-`define ADDER_TREE_BITWIDTH `NN_BITWIDTH+`NUM_NH_LAYERS
-`define ADDER_TREE_WIDTH `ADDER_TREE_BITWIDTH
-`define MEAN_DIVSION_CONSTANT `ADDER_TREE_WIDTH'd`NEIGHBORHOOD_SIZE
-
-// Softmax 
-`define SOFTMAX_IN_VECTOR_LENGTH ((`FEATURE_SIZE * `FEATURE_SIZE) / `NEIGHBORHOOD_SIZE ) * `NUM_KERNELS  // the number of inputs to the softmax layer
-`define NUM_CLASSES 10// number of output classes for the entire nn
-
-// Matrix multiply (for Softmax)
-`define NUM_INPUT_IM 1 // The number of images input to the layer at a time
-`define NUM_INPUT_N  `NUM_KERNELS * `FEATURE_SIZE * `FEATURE_SIZE // The number of input neurons to the layer
-`define NUM_OUTPUT_N `NUM_CLASSES
-`define FFN_WIDTH `NN_WIDTH // The width of the inputs to the feed forward network. Should be the same as the output width of the softmax layer.
-`define FFN_BITWIDTH `FFN_WIDTH - 1
-`define FFN_OUT_WIDTH `FFN_WIDTH * 2 + `LOG2(`NUM_INPUT_N) // The width of the outputs of the feed forward network
-//`define FFN_OUT_WIDTH PFFN_OUT_WIDTH
-`define FFN_OUT_BITWIDTH `FFN_OUT_WIDTH - 1 
-`define SUM_WIRE_LEN ( `NUM_INPUT_N * 2 ) - 1 // The number of indexes in the adder tree vector
-
-// Normalization (for Softmax)
-`define NORM_IN_WIDTH `FFN_OUT_WIDTH
-`define NORM_IN_BITWIDTH `NORM_IN_WIDTH - 1
-`define NUM_NORM_LAYERS `LOG2(`NUM_CLASSES)
-//`define NUM_NORM_LAYERS PNUM_NORM_LAYERS
-`define NORM_OUT_WIDTH (`NORM_IN_WIDTH + `NUM_NORM_LAYERS)
-`define NORM_OUT_BITWIDTH `NORM_OUT_WIDTH - 1
-
-
-`define LOG2(x) \
-    (x <= 2) ? 1 : \
-    (x <= 4) ? 2 : \
-    (x <= 8) ? 3 : \
-    (x <= 16) ? 4 : \
-    (x <= 32) ? 5 : \
-    (x <= 64) ? 6 : \
-    (x <= 128) ? 7 : \
-    (x <= 256) ? 8 : \
-    ((x) <= 512) ? 9 : \
-    (x <= 1024) ? 10 : \
-    (x <= 2048) ? 11 : \
-    (x <= 4096) ? 12 : \
-    (x <= 8192) ? 13 : \
-    (x <= 16384) ? 14 : \
-    (x <= 32768) ? 15 : \
-    -100000
-	 
-
+`define SUM_WIRE_LEN 7999
+`define NUM_OUTPUT_N 4
+`define NORM_IN_BITWIDTH 43
+`define KERNEL_SIZE 9
--- Hardware/v/softmax/normalize.v
+++ Hardware/v/softmax/normalize.v
@@ -23,21 +23,22 @@ genvar i;
 generate
 for(i = 0; i < `NUM_CLASSES; i=i+1) begin : connect_in_vector
     assign in_vector_wire[i] = in_vector[(`NN_WIDTH*i)+`NN_BITWIDTH:`NN_WIDTH*i];
-    assign adder_tree_wire[i+`NUM_CLASSES-1] = in_vector[(`NN_WIDTH*i)+`NN_BITWIDTH:`NN_WIDTH*i];
+    assign adder_tree_wire[i+`NUM_CLASSES-1] = { `ADDER_TREE_PAD'd0, in_vector[(`NN_WIDTH*i)+`NN_BITWIDTH:`NN_WIDTH*i] };
   end
 endgenerate
 
 // sum inputs
 genvar j;
 generate
-for(j= (`NUM_CLASSES*2)-1 ; j <=2 ; j=j-2) begin : sum_inputs
-  add2 add2_inst(
+for(j= (`NUM_CLASSES*2)-2 ; j >=1 ; j=j-2) begin : sum_inputs
+  norm_add2 add2_inst(
     .clock(clock),
     .reset(reset),
-    .opperand_a(adder_tree_wire[j-1]),
-    .opperand_b(adder_tree_wire[j]),
+    .operand_a(adder_tree_wire[j-1]),
+    .operand_b(adder_tree_wire[j]),
     .sum(adder_tree_wire[(j/2)-1])
   );  
+//assign adder_tree_wire[(j/2)-1] = adder_tree_wire[j] + adder_tree_wire[j-1];
 end // for
 endgenerate
 
@@ -45,13 +46,18 @@ endgenerate
 genvar k;
 generate
 for (k=0; k < `NUM_CLASSES; k=k+1) begin : divide_by_sum
+/*
   always@(posedge clock or negedge reset) begin
     if(reset == 1'b0) begin
       out_vector_wire[k] <= `NORM_OUT_BITWIDTH'd0;
     end else begin 
-      out_vector_wire[k] <= in_vector_wire[k] / adder_tree_wire[1];
+      out_vector_wire[k] <= in_vector_wire[k] / adder_tree_wire[0];
     end
   end // always
+*/
+  always@(posedge clock) begin
+    out_vector_wire[k] <= in_vector_wire[k] / adder_tree_wire[0];
+    end
 end // for
 endgenerate
 
@@ -66,3 +72,25 @@ endgenerate
 
 endmodule
 
+module norm_add2(
+  input clock,
+  input reset,
+
+  input [`NORM_OUT_BITWIDTH:0] operand_a,
+  input [`NORM_OUT_BITWIDTH:0] operand_b,
+  output reg[`NORM_OUT_BITWIDTH:0] sum 
+);
+
+`ifdef NORM_RESET
+always@(posedge clock or negedge reset) begin
+  if(reset == 1'b0) 
+    sum <= `NORM_OUT_WIDTH'd0;
+  else 
+    sum <= operand_a + operand_b;
+end // always
+`else
+always@(posedge clock) begin
+  sum <= operand_a + operand_b;
+end // always
+`endif
+endmodule
