fe37262c662af27bfb228f561e544cc6e990354d
Alan Ehret <alan.4186@yahoo.com>
2016-10-06 16:24:55 -0400

    removed 2d shifting except for bottom row

--- Hardware/v/shifting_window.v
+++ Hardware/v/shifting_window.v
@@ -7,10 +7,12 @@
 // left corner.
  
 `include "../network_params.h"
-module shifting_buffer(
+module shifting_window(
   input clock,
   input reset,
-  input [`PIXEL_BITWIDHT:0] in,
+  input shift_up, // shift all rows up
+  input shift_left, // to load new pixel into bottom row
+  input [`CAMERA_PIXEL_BITWIDTH:0] pixel_in,
   output [`BUFFER_OUT_VECTOR_BITWIDTH:0] buffer_out 
 );
 
@@ -18,23 +20,21 @@ module shifting_buffer(
 
 
 // wire declarations
-wire [`PIXEL_WIDTH] buffer_wire [`BUFFER_BW:0][`BUFFER_BH:0]
+wire [`CAMERA_PIXEL_BITWIDTH:0] buffer_wire [`BUFFER_BW:0][`BUFFER_BH:0];
 
 // reg declarations
 
 
-  genvar i
-  genvar j
+  genvar i;
+  genvar j;
   generate
-    for(j=1;j < `BUFFER_H; j=j+1) begin
-      for(i=1; i < `BUFFER_W; i=i+1) begin
+    for(j=1;j < `BUFFER_H; j=j+1) begin : core_buffer_h_loop
+      for(i=0; i < `BUFFER_W; i=i+1) begin : core_buffer_w_loop
         buffer_unit unit_inst(
           .clock(clock),
           .reset(reset),
-          .shift_en(shift_en),
-          .shift_dir(shift_dir),
-          .in_a(buffer_wire[i-1][j]), // right to left (width)
-          .in_b(buffer_wire[i][j-1]), // bottom to top (height)
+          .shift(shift_up),
+          .in(buffer_wire[i][j-1]), // bottom to top (height) 
           .out(buffer_wire[i][j])
         );
       end
@@ -44,42 +44,26 @@ wire [`PIXEL_WIDTH] buffer_wire [`BUFFER_BW:0][`BUFFER_BH:0]
   // loop over first row of width and height
   genvar k;
   generate 
-  for (k = 1; k < BUFFER_W; k=k+1) begin : width_row0_loop
+  for (k = 1; k < `BUFFER_W; k=k+1) begin : width_row0_loop
     buffer_unit w_row0_unit_inst(
       .clock(clock),
       .reset(reset),
-      .shift_en(shift_en),
-      .shift_dir(shift_dir),
-      .in_a(buffer_wire[k-1][0]), // right to left (width)
-      .in_b(`CAMERA_PIXEL_WIDTH'd0), // bottom to top (height)
+      .shift(shift_left),
+      .in(buffer_wire[k-1][0]), // right to left (width)
       .out(buffer_wire[k][0])
     );
   end // for
 
-  for (k = 1; k < BUFFER_H; k=k+1) begin : height_row0_loop
-    buffer_unit h_row0_unit_inst(
-      .clock(clock),
-      .reset(reset),
-      .shift_en(shift_en),
-      .shift_dir(shift_dir),
-      .in_a(`CAMERA_PIXEL_WIDTH'd0), // right to left (width)
-      .in_b(buffer_wire[0][k-1]), // bottom to top (height)
-      .out(buffer_wire[0][k])
-    );
-  end // for 
   endgenerate
 
   // instantiate origin window unit
   buffer_unit origin_unit_inst(
       .clock(clock),
       .reset(reset),
-      .shift_en(shift_en),
-      .shift_dir(shift_dir),
-      .in_a(pixel_in), // right to left (width)
-      .in_b(`CAMERA_PIXEL_WIDTH'd0), // bottom to top (height)
-      .out(buffer_wire[1][0])
+      .shift(shift_left),
+      .in(pixel_in), // right to left (width)
+      .out(buffer_wire[0][0])
     );
-  end // for 
  
 
 
@@ -90,31 +74,36 @@ wire [`PIXEL_WIDTH] buffer_wire [`BUFFER_BW:0][`BUFFER_BH:0]
   generate
     for (n=0; n < `BUFFER_H; n=n+1) begin : buffer_height_loop
       for (m=0; m < `BUFFER_W; m=m+1) begin : buffer_width_loop
-        assign buffer_wire[m][n] = buffer_out[ \ 
-        (`CAMERA_PIXEL_WIDTH*m)+(`BUFFER_W*CAMERA_PIXEL_WIDTH*n) +`CAMERA_PIXEL_BITWIDTH:\
-          (`CAMERA_PIXEL_WIDTH*m)+(`BUFFER_W*CAMERA_PIXEL_WIDTH*n) \ 
+        /*
+        assign buffer_wire[m][n] = buffer_out[
+        (`CAMERA_PIXEL_WIDTH*m)+(`BUFFER_W*`CAMERA_PIXEL_WIDTH*n) +`CAMERA_PIXEL_BITWIDTH:
+          (`CAMERA_PIXEL_WIDTH*m)+(`BUFFER_W*`CAMERA_PIXEL_WIDTH*n)
           ];
-  endmodule
+        */
+       assign buffer_out[
+        (`CAMERA_PIXEL_WIDTH*m)+(`BUFFER_W*`CAMERA_PIXEL_WIDTH*n) +`CAMERA_PIXEL_BITWIDTH:
+          (`CAMERA_PIXEL_WIDTH*m)+(`BUFFER_W*`CAMERA_PIXEL_WIDTH*n)
+          ] = buffer_wire[m][n];
+      end // for m
+    end // for n
+  endgenerate
+
+endmodule
   
   
-  module buffer_unit(
-    input clock,
-    input reset,
-    input shift_en,
-    input shift_dir,
-    input [`PIXEL_BITWIDTH:0] in_a,
-    input [`PIXEL_BITWIDTH:0] in_b,
-    output [`PIXEL_BITWIDTH:0] out
-  );
-    always@(posedge clock or negedge reset) begin
-      if(reset == 1'b0)
-        out <= `PIXEL_WIDTH'd0;
-      else if(shift_en) begin
-        if(shift_mode)
-          out <= in_a;
-        else
-          out <= in_b;
-      end else 
+module buffer_unit(
+  input clock,
+  input reset,
+  input shift,
+  input [`CAMERA_PIXEL_BITWIDTH:0] in,
+  output reg [`CAMERA_PIXEL_BITWIDTH:0] out
+);
+  always@(posedge clock or negedge reset) begin
+    if(reset == 1'b0)
+      out <= `CAMERA_PIXEL_WIDTH'd0;
+    else if(shift) 
+        out <= in;
+      else
         out <= out;
-    end // always
-  endmodule
+  end // always
+endmodule
