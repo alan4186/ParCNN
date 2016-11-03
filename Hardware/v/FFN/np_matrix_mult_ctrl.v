b79b79f2e4629f75bbe10248840a3c300d026312
Alan Ehret <alan.4186@yahoo.com>
2016-11-03 13:09:52 -0400

    added ready output, combind address counters

--- Hardware/v/FFN/np_matrix_mult_ctrl.v
+++ Hardware/v/FFN/np_matrix_mult_ctrl.v
@@ -3,23 +3,33 @@ module np_matrix_mult_ctrl(
   input reset,
 
   input buffer_rdy,
-
-  output [`FFN_BITWIDTH:0] fm_addr,
-  output [`WEIGHT_BITWIDTH:0] weight_addr,
+  
+  output [`FFN_BITWIDTH:0] addr,
+  output product_rdy
 );
 
 always @(posedge clock or negedge reset) begin
   if (reset == 1'b0) begin
-    fm_addr <= `FFN_WIDTH'd0;
-    weight_addr <= `WEIGHT_WIDTH'd0;
+    addr <= `FFN_WIDTH'd0;
   end else begin
     if (buffer_rdy) begin
-      fm_addr <= fm_addr + `FFN_WIDTH'd1;
-      weight_addr <= weight_add + `WEIGHT_WIDTH'd1;
+      addr <= addr + `FFN_WIDTH'd1;
     end else begin
-      fm_addr <= `FFN_WIDTH'd0;
-      weight_addr <= `WEIGHT_WIDTH'd0;
+      addr <= `FFN_WIDTH'd0;
     end // rdy
+  end // reset
+end // always
+
+
+always@(posedge clock or negedge reset) begin
+  if(reset == 1'b0) begin
+    product_rdy <= 1'b0;
+  end else begin
+    if(addr == `ADDR_MAX) begin
+      product_rdy <= 1'b1;
+    end else begin
+      product_rdy <= 1'b0;
+    end // addr max
   end // reset
 end // always
 
