a8e9b1035fde05caa9127a4e9434faa16539a20e
Alan Ehret <alan.4186@yahoo.com>
2016-11-14 21:24:28 -0500

    create script to instantiate ffn weight rams

---
+++ Software/ffn_weight_ram.py
@@ -0,0 +1,26 @@
+import network_params
+
+ram_str = ''
+ram_init = ''
+module_name = 'weight_ram'
+file_name = '../Hardware/v/ffn_weight_rams.h'
+
+for i in range(0,network_params.NUM_KERNELS):
+    inst_name = module_name + '_inst' + str(int(i)) 
+    index_max = (network_params.FFN_IN_WIDTH * i) + network_params.FFN_IN_BITWIDTH
+    index_min = network_params.FFN_IN_WIDTH * i
+    ram_init = module_name + ' ' + inst_name  
+    ram_init = ram_init +  """ (
+    .clock(clock),
+//  .data(),
+  .rdaddress(fm_rd_addr),
+//  .wraddress(),
+  .wren(1'b0),\n"""
+    ram_init = ram_init + "  .q(w_buffer_data_vector["+str(index_max) + ':' + str(index_min)+"])\n);\ndefparam "
+ 
+    ram_init = ram_init + inst_name + '.init_file = "../../Software/ffn_weight_mifs/ffn_weight' + str(int(i)) + '.mif";\n\n'
+
+    ram_str = ram_str + ram_init
+
+with open(file_name, 'w') as f:
+    f.write(ram_str)
