1bb5593bb7a52ec01e02421b82094ac9d3acb89c
Alan Ehret <alan.4186@yahoo.com>
2017-01-06 01:11:33 -0500

    remove paths for files that wont be used in this project

--- Software/python/project_settings.py
+++ Software/python/project_settings.py
@@ -4,15 +4,6 @@ import os.path
 # Append to this to create absolute paths for the  other locations
 project_root = os.path.expanduser('~/Documents/co_processors/ParCNN/')
 
-# Output location for the .h file used in verilog
-network_params = project_root + 'Hardware/network_params.h'
-
-# Output location for the read port mux .v file
-read_port_mux = project_root + 'Hardware/v/read_port_mux.v'
-
-# Output location for window selector .v file
-window_selector = project_root + 'Hardware/v/window_selector.v'
-
 # Directory with Kernel CSVs 
 kernel_path = project_root + 'Software/Verilog_Builder/kernel_base2/'
 
