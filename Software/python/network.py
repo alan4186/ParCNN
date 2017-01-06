e5dc895ec38ac008805e4e594b26ab01890c962a
Alan Ehret <alan.4186@yahoo.com>
2017-01-06 01:09:16 -0500

    add parameter computation to ConvLayer class

--- Software/python/network.py
+++ Software/python/network.py
@@ -5,15 +5,16 @@ class Net:
     def __init__(self):
         self.layers = OrderedDict()
 
-    def add_conv(self, name, x_size, y_size, num_kernels):
-        
+    def add_conv(self, name, kx_size, ky_size, num_kernels, ix_size, iy_size, sharing_factor):
+        self.layers[name] = ConvLayer(kx_size,ky_size,num_kernels,ix_size,iy_size,sharing_factor)
+
     def add_relu(self):
 
     def add_max_pool(self):
 
     def add_dense(self):
 
-
+    def export():
 
 
 
@@ -24,18 +25,34 @@ class InputLayer:
     def __init__(self, x_size, y_size):
         self.x_size = x_size
         self.y_size = y_size
+
 class ConvLayer:
 
     def __init__(self, kx_size, ky_size, num_kernels, ix_size, iy_size, sharing_factor):
-        self.kx_size = x_size
-        self.ky_size = y_size
-        self.num_kernels = num_kernels
-        self.ix_size = ix_size
-        self.iy_size = iy_size
-        self.sharing_factor = sharing_factor
+        self.layer_type = 'conv'
+        # make sure the kernel size is at least 1 
+        # pixel smaller than the input in the x dimension and 
+        # the same size as the input in the y dimension
+        if ix_size-kx_size < 1:
+            raise ValueError('The kernel X dimension must' 
+                    + 'be at least 1 less than the input X'
+                    + 'dimension. Kernel X size: '
+                    + str(kx_size)', Input X size: ' +str(ix_size)
 
-        # Compute properties for network
+        if iy_size-ky_size <0:
+            raise ValueError('The kernel Y dimension must' 
+                    + 'be less than or equal to the input Y'
+                    +  'dimension. Kernel Y size: '
+                    + str(ky_size)', Input Y size: ' +str(iy_size))
 
+        # compute parameters
+        # tree sharing is not implemented yet, each kernel gets its own tree
+        self.NUM_TREES = num_kernels 
+        self.P_SR_DEPTH = kx_size
+        self.RAM_SR_DEPTH = ix_size - kx_size
+        self.NUM_SR_ROWS = ky_size
+        self.MA_TREE_SIZE = 8 * kx_size * ky_size
+        
 
 
 class ReluLayer:
