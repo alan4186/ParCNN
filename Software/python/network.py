102e1ad1be4dd64e2820003f7178d5476b9f9b51
Alan Ehret <alan.4186@yahoo.com>
2017-02-09 23:17:54 -0500

    add tensorflow implementation of network, wip

--- Software/python/network.py
+++ Software/python/network.py
@@ -4,10 +4,14 @@ import math
 
 from convLayer import ConvLayer
 # A class to describe the network that will be implemented in hardware
-class Net(object):
+class Net:
 
-    def __init__(self):
+    def __init__(self, project_name, steps):
         self.layers = OrderedDict()
+        self.project_name = project_name
+        
+        # Training settings
+        self.training_steps = steps
 
     def add_conv(self, name, kx_size, ky_size, kz_size, num_kernels, ix_size, iy_size, iz_size, sharing_factor, rq_max, rq_min, kernels):
         self.layers[name] = ConvLayer(name,kx_size,ky_size,kz_size,num_kernels,ix_size,iy_size,iz_size,sharing_factor, rq_max, rq_min, kernels)
@@ -18,10 +22,10 @@ class Net(object):
     def add_max_pool(self):
         print 'under construction'
 
-    def export(self):
-        print 'under construction'
+        
+    def export_cnn_module(self):
+        #TODO create project directory
 
-    def write_cnn_module(self):
         cnn_module = ''
         port_list = \
 """module cnn (
@@ -43,14 +47,83 @@ output [7:0] pixel_out
         # instantiate layer modules
         for pair in self.layers.items():
             v = pair[1]
-            inst = v.write_inst(pair[0], wire_index, wire_index+1)
+            #inst = v.write_inst(pair[0], wire_index, wire_index+1)
+            inst = v.export(pair[0], wire_index, wire_index+1)
             wire_index += 1 
             cnn_module += inst
 
         cnn_module +="\nassign pixel_out = wire8["+str(wire_index)+"];\n\n"
 
         cnn_module += "endmodule"
+        with open("../"+self.project_name+"cnn.v",'w') as f:
+            f.write(cnn)
+        
         return cnn_module
+
+    def train(self):
+
+        # TODO get training data
+        
+        # TODO split commands dictionary into seperate functions in the respective classes
+        # Create dictionary to translate layer classes into functions
+        commands = { 
+            'conv':self.conv2d,
+            'relu':tf.nn.relu,
+            'bias':add_bias,
+            'max_pool':self.max_pool
+            } 
+
+        # Build the Tensorflow graph
+        #TODO parametric input size
+        input_placeholder = tf.placeholder(tf.float32, shape=[None, 784])
+        layer_outputs = [input_placeholder]
+        for l in self.layers:
+            layer_outputs.append(commands[l.layer_type](layer_outputs[-1],l.tf_var))
+            
+
+        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, y_))
+        train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+        correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        init_op = tf.global_variables_initializer()
+
+        with tf.Session() as sess:
+            init_op.run()
+            
+            # start training
+            for i in range(self.training_steps):
+                batch = mnist.train.next_batch(50)
+                if i%100 == 0:
+                    train_accuracy = accuracy.eval(feed_dict={
+                        x:batch[0], y_: batch[1] })
+                    print("step %d, training accuracy %g"%(i, train_accuracy))
+                    train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+
+            print("test accuracy %g"%accuracy.eval(feed_dict={
+                x: mnist.test.images, y_: mnist.test.labels}))
+
+            #TODO save network
+
+    def conv2d(self,x,W):
+        # x: the input to the convolutional layer
+        # W: the tensorflow weight parameters of the layer
+        return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='VALID')
+
+    def add_bias(self,h, bias):
+        # h: the input to the layer
+        # bias: the tensorflow variable to add to the input
+        return bias+h
+
+    def max_pool(self, x,dims):
+        # x: the tensorflow input to the layer 
+        # dims: an x/y tuple representing the pool dimensions
+
+        #TODO implement variable dimension pools
+        return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+            strides=[1, 2, 2, 1], padding='VALID')
+
+    def set_train_steps(self,steps):
+        self.training_steps = steps
 
 class InputLayer:
     
