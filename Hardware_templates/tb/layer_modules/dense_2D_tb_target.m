fd61a0b3b495e161d3c7269ce29f73dc5ec00687
Alan Ehret <alan.4186@yahoo.com>
2017-02-13 23:05:49 -0500

    add matlab scripts to compute tb outputs

---
+++ Hardware_templates/tb/layer_modules/dense_2D_tb_target.m
@@ -0,0 +1,56 @@
+% Convolution Test Bench calculation
+
+Kernel1 = [ 1 1 2 2; 1 1 2 2; 2 2 1 1; 2 2 1 1 ]
+Kernel2 = [ 2 2 3 3; 2 2 3 3; 2 2 3 3; 2 2 3 3 ]
+
+window_a = [15 14 13 12; 11 10 9 8; 7 6 5 4; 3 2 1 0 ]
+window_b = [16 15 14 13; 12 11 10 9; 8 7 6 5; 4 3 2 1 ]
+
+a1 = sum(sum(Kernel1 .* window_a))
+a2 = sum(sum(Kernel2 .* window_a))
+b1 = sum(sum(Kernel1 .* window_b))
+b2 = sum(sum(Kernel2 .* window_b))
+
+%{
+
+Kernel1 =
+
+     1     1     2     2
+     1     1     2     2
+     2     2     1     1
+     2     2     1     1
+
+
+Kernel2 =
+
+     2     2     3     3
+     2     2     3     3
+     2     2     3     3
+     2     2     3     3
+
+
+window_a =
+
+    15    14    13    12
+    11    10     9     8
+     7     6     5     4
+     3     2     1     0
+
+
+window_b =
+
+    16    15    14    13
+    12    11    10     9
+     8     7     6     5
+     4     3     2     1
+
+
+a1 = 180
+
+a2 = 292
+
+b1 = 204
+
+b2 = 332
+
+%}
