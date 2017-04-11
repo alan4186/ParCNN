55c9e45f7d356c61e77cfe2f07a8db550b89da54
Alan Ehret <alan.4186@yahoo.com>
2017-04-10 20:26:09 -0400

    add quantize functions

---
+++ Software/quantize_def.py
@@ -0,0 +1,35 @@
+import numpy as np
+import math
+import tensorflow as tf
+
+def quantize(f, f_min, f_max, q_bw):
+    bits = 2**q_bw
+    f_range = f_max - f_min
+    q = np.round( (f - f_min) * bits / f_range)
+    q_offset = np.round( f_min * bits / f_range)
+    return q + q_offset
+
+def dequantize(q, f_min, f_max, q_bw):
+    bits = 2**q_bw
+    f_range = f_max - f_min
+    sc = f_range / bits
+    return q * sc * sc
+
+
+
+def tf_quantize(f, f_min, f_max, q_bw):
+    bits = tf.pow(2.0, q_bw)
+    f_range = f_max - f_min
+    q = tf.round( (f - f_min) * bits / f_range)
+    q_offset = tf.round( f_min * bits / f_range)
+    return q + q_offset
+
+def tf_dequantize(q, f_min, f_max, q_bw):
+    bits = tf.pow(2.0, q_bw)
+    f_range = f_max - f_min
+    sc = f_range / bits
+    return q * sc * sc
+
+
+def tf_reluq(t,zero):
+    return tf.maximum(t,zero)
