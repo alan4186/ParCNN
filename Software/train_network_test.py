0c70290b79e44d78eb6bb6df3c1d83c6d2a8404e
Alan Ehret <alan.4186@yahoo.com>
2017-04-12 23:44:12 -0400

    added simple training test script

---
+++ Software/train_network_test.py
@@ -0,0 +1,13 @@
+import network
+
+nn = network.Net('test',100000)
+
+nn.add_conv('c1',7,7,1,8,28,28,1,1,10,-10)
+nn.add_bias('b1',8)
+nn.add_relu('r1',8,10,-10)
+nn.add_dense('fc',22,22,8,10,1,10,-10)
+nn.add_bias('bfc',10)
+
+nn.train()
+
+nn.export_cnn_module()
