78436611d7c8c14ce738c9e8160daa83fd647a6e
Alan Ehret <alan.4186@yahoo.com>
2017-04-25 22:37:50 -0400

    added docstrings to quantize ops

--- Software/hw_quantize_ops.py
+++ Software/hw_quantize_ops.py
@@ -3,6 +3,31 @@ import math
 import tensorflow as tf
 
 def tf_quantize(f, f_min, f_max, q_bw):
+    """ Convert floating point tensor to quantized representation
+
+    Quantize the floating point tensor to a signed interger representation. 
+    Usually, an 8 bit representation will be used yielding a range of -128
+    to 127.  The returned tensor's type is still float32 but its values will
+    be integers.
+
+    The integers represent floating point values lineraly spaced between
+    f_min and f_max. The range given by f_min and f_max must be symetric.
+
+    Args:
+        f: A tensor of type float32
+        f_min: A float32 tensor scalar representing the minimum floating 
+            point value.  This argument can be converted from an int or
+            float to a tensor by tensorflow.
+         f_max: A float32 tensor scalar representing the maximum floating 
+            point value.  This argument can be converted from an int or
+            float to a tensor by tensorflow.
+         q_bw: The bitwidth of the quantized value.  Can be of type float,
+            int or tensor
+    Returns:
+        A float32 tensor the same size as f that has been rounded to 
+        integers.
+    """
+
     bits = tf.pow(2.0, q_bw) - 1
     f_range = f_max - f_min
     q = tf.round( (f - f_min) * bits / f_range)
@@ -10,7 +35,25 @@ def tf_quantize(f, f_min, f_max, q_bw):
     return q + q_offset
 
 def tf_dequantize(q, f_min, f_max, q_bw):
-    bits = tf.pow(2.0, q_bw) - 1
+    """ Convert a quantized tensor to its floating point representation
+
+    Convert the quantized (but still float32) tensor to the floating point 
+    representation. See tf_quantize for details on the range of the
+    quantized tensor.
+
+    Args:
+        q: A quantized tensor of type float32
+        f_min: A float32 tensor scalar representing the minimum floating 
+            point value.  This argument can be converted from an int or
+            float to a tensor by tensorflow.
+        f_max: A float32 tensor scalar representing the maximum floating 
+            point value.  This argument can be converted from an int or
+            float to a tensor by tensorflow.   bits = tf.pow(2.0, q_bw) - 1
+
+    Returns:
+        A float32 tensor the same size as q
+
+    """
     f_range = f_max - f_min
     sc = f_range / (bits)
     f = q - tf.round(f_min/sc)
@@ -20,6 +63,40 @@ def tf_dequantize(q, f_min, f_max, q_bw):
 
 
 def tf_requantize(q32,input_max,output_max,in_bw,out_bw, clamp=True):
+    """ Convert a tensor to a new bitwidth and range
+
+    Convert the new tensor to a new bitwidth and range.  This is usefull for
+    reducing the output bitwidth for layers with add or multiply operations.
+    Adding or multiplying will increase the bitwidth, this function can
+    convert it back to 8 bits and change the range of the quantized tensor
+    to utilize more of the quantization range after a convolution.
+
+    The arguments in_bw does not need to be an integer. The bitwidth of a 
+    quantized tensor will change when it is multiplied or added.  For 
+    convolution the bitwidth is (8*2)-1 + log2(num_elements_in_kernel). In
+    most cases this will not be an integer.  For example: a quantized
+    convolution with a 5x5 kernel of ones on an image of ones will have an
+    output that has a bitwidth of (8*2)-1 + log2(25) = 19.64
+
+    Args:
+        q32: A quantized tensor to change the range and bitwidth of.
+        input_max: A float or tensor representing the maximum of the
+            symetric floating point range of the input tensor q32.
+        output_max: A float or tensor representing the maximum of the
+            symetric floating point range of the output tensor.
+        in_bw: A float or tensor representing the bitwidth of the input 
+            tensor q32.  Does not need to be an integer.
+        out_bw: A float or tensor representing the bitwidth of the output
+            tensor.  Should almost always be 8.0.
+
+    Kwargs:
+        clamp: A boolean.  If true, the output tensor will be limited to a 
+            range of -128 to 127.
+
+    Returns:
+        q8: A quantized float32 tensor in the new bitwidth and range.
+
+    """
     bits_in = (2**in_bw) - 1
     bits_out = (2**out_bw) - 1
     q8 = tf.multiply(q32, input_max / output_max * bits_out / bits_in)
@@ -33,6 +110,11 @@ def tf_reluq(t,zero):
     return tf.maximum(t,zero)
 
 def conv_max(mx, num_elements):
+    """ Compute the maximum posible output of a Convolution
+
+    The max value is based on the maximum of the symetric quantized range
+
+    """
     return mx**2 * num_elements
 
 
