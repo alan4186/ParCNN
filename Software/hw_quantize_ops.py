398605e0536b71e763faf113e6da2229b4dc2aa1
Alan Ehret <alan.4186@yahoo.com>
2017-04-25 22:00:48 -0400

    delete numpy quantize functions

--- Software/hw_quantize_ops.py
+++ Software/hw_quantize_ops.py
@@ -2,21 +2,6 @@ import numpy as np
 import math
 import tensorflow as tf
 
-def quantize(f, f_min, f_max, q_bw):
-    bits = (2**q_bw) - 1
-    f_range = f_max - f_min
-    q = np.round( (f - f_min) * bits / f_range)
-    q_offset = np.round( f_min * bits / f_range)
-    return q + q_offset
-
-def dequantize(q, f_min, f_max, q_bw):
-    bits = (2**q_bw) - 1
-    f_range = f_max - f_min
-    sc = f_range / bits
-    return q * sc * sc
-
-
-
 def tf_quantize(f, f_min, f_max, q_bw):
     bits = tf.pow(2.0, q_bw) - 1
     f_range = f_max - f_min
