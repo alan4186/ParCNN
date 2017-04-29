f6505e46818cf6530d19af8d4a8668f0fe2ac434
Alan Ehret <alan.4186@yahoo.com>
2017-04-29 17:36:19 -0400

    fix syntax errors in requantize

--- Hardware_templates/v/layer_modules/requantize.v
+++ Hardware_templates/v/layer_modules/requantize.v
@@ -16,20 +16,17 @@ wire [7:0] q8 [SIZE-1:0];
 
 reg [7:0] q8_clipped [SIZE-1:0];
 
-assign ones = (32+8+SHIFT){1'b1};
-assign zeros = (32+8+SHIFT){1'b0};
+assign ones = {(32+8+SHIFT){1'b1}};
+assign zeros = {(32+8+SHIFT){1'b0}};
 
 
 genvar i;
 generate
 for(i=0; i<SIZE; i=i+1) begin : rq_loop
-  always@(*) begin
-    if( SHIFT > 0 ) begin
-      {remainder[i], q8[i]} = pixel_in[i] << SHIFT;
-    end else begin
-      {remainder[i], q8[i]} = pixel_in[i][31:0-SHIFT];
-    end
+  assign {remainder[i], q8[i]} = (SHIFT>0) ? pixel_in[32*i+31:32*i] << SHIFT : pixel_in[32*i+31:32*i-SHIFT];
 
+
+  always@(*) begin
     if(remainder[i][32-8+SHIFT]) begin
       q8_clipped[i] = 8'd128; // -128 in 2's compliment
     end else begin
@@ -38,8 +35,8 @@ for(i=0; i<SIZE; i=i+1) begin : rq_loop
 
   end // always
 
-  always@(posedge clk) begin
-    if(remainder[i] == ones | remainer[i] == zeros) begin
+  always@(posedge clock) begin
+    if(remainder[i] == ones | remainder[i] == zeros) begin
       pixel_out[i] <= q8[i];
     end else begin
       pixel_out[i] <= q8_clipped[i];
