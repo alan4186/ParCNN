b72a6da872d0834e4aaf647431cd9d34c0fb6217
Alan Ehret <alan.4186@yahoo.com>
2017-04-30 15:27:18 -0400

    change output width of 8bit add to 9 bits

--- Hardware_templates/v/layer_modules/bias.v
+++ Hardware_templates/v/layer_modules/bias.v
@@ -9,25 +9,16 @@ module bias #(
   output [9*SIZE-1:0] sum
 );
 
-wire [SIZE-1:0] c;
-wire [8*SIZE-1:0] sum_8; // the 8 bit sum of the addition, only 7 msb needed
-
 genvar i;
 generate
 for(i=0; i<SIZE; i=i+1) begin : bias_loop
   add_8bit_signed add_inst (
     .clock(clock),
-    .dataa(a[i*8+7:i*8]),
-    .datab(b[i*8+7:i*8]),
-    .cout(c[i]),
-    .result(sum_8[i*8+7:i*8])
+    .dataa({a[8*i+7], a[i*8+7:i*8]}), // extend sign bit for 9bit input
+    .datab({b[8*i+7], b[i*8+7:i*8]}),
+    .result(sum[i*9+8:i*9])
   );
 end //for
-
-// wire up carry bit
-for(i=0; i<SIZE; i=i+1) begin : carry_loop
-  assign sum[i*9+8:i*9] = {c[i], sum_8[i*8+7:i*8]};
-end // for
 
 endgenerate
 
