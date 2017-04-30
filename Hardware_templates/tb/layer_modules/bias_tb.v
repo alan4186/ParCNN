0ab08c58795abf4263cd5afeac6e1b29c014d073
Alan Ehret <alan.4186@yahoo.com>
2017-04-30 15:28:38 -0400

    add signed values to test bias test bench

--- Hardware_templates/tb/layer_modules/bias_tb.v
+++ Hardware_templates/tb/layer_modules/bias_tb.v
@@ -8,7 +8,7 @@ reg reset;
 
 reg [8*SIZE-1:0] a;
 reg [8*SIZE-1:0] b;
-wire [8*SIZE-1:0] out;
+wire [9*SIZE-1:0] out;
 
 // DUT
 bias #(
@@ -45,32 +45,34 @@ initial begin
   
   #10
   $display("Time = %0d",$time);
-  if (out == {8'd1, 8'd2, 8'd3, 8'd4})
-    $display("out = %0d\n\t\t\tPASS!", out);
+  if (out == {9'd2, 9'd4, 9'd6, 9'd8})
+    $display("out = %h\n\t\t\tPASS!", out);
   else
-    $display("out = %0d\n\t\t\tFAIL!", out);
+    $display("out = %h\n\t\t\tFAIL!", out);
 
   // next values
-  a = {8'd31, 8'd28,8'd53,8'd94};
-  b = {8'd1, 8'd2,8'd3,8'd4};
+  a = {8'he1, 8'h9c,8'd53,8'd94};
+  b = {8'hff, 8'd2,8'hfb,8'd4};
 
   #10
   $display("Time = %0d",$time);
-  if (out == {8'd16, 8'd15, 8'd28, 8'd49})
-    $display("out = %0d\n\t\t\tPASS!", out);
+  $display("Target = %h",{9'h1e0, 9'h19e, 9'd48, 9'd98});
+  if (out == {9'h1e0, 9'h19e, 9'd48, 9'd98})
+    $display("out = %h\n\t\t\tPASS!", out);
   else
-    $display("out = %0d\n\t\t\tFAIL!", out);
+    $display("out = %h\n\t\t\tFAIL!", out);
 
   // next values
-  a = {8'd128, 8'd128,8'd200,8'd255};
+  a = {8'h80, 8'd128,8'd200,8'd255};
   b = {8'd127, 8'd128,8'd200,8'd255};
 
   #10
   $display("Time = %0d",$time);
-  if (out == {8'd127, 8'd128, 8'd200, 8'd255})
-    $display("out = %0d\n\t\t\tPASS!", out);
+  $display("Target = %h",{9'h1ff, 9'h100, 9'h190, 9'h1fe});
+  if (out == {9'h1ff, 9'h100, 9'h190, 9'h1fe})
+    $display("out = %h\n\t\t\tPASS!", out);
   else
-    $display("out = %0d\n\t\t\tFAIL!", out);
+    $display("out = %h\n\t\t\tFAIL!", out);
 
   #80
   $display("\n");
