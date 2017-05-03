25e186caeb9a7f094d7405eab278e0c228864283
Alan Ehret <alan.4186@yahoo.com>
2017-05-03 01:55:58 -0400

    delete unused megafunctions

--- Hardware_templates/v/components/megafunctions/add_32bit_inst.v
+++
@@ -1,6 +0,0 @@
-add_32bit	add_32bit_inst (
-	.clock ( clock_sig ),
-	.dataa ( dataa_sig ),
-	.datab ( datab_sig ),
-	.result ( result_sig )
-	);
--- Hardware_templates/v/components/megafunctions/mult_8bit_inst.v
+++
@@ -1,6 +0,0 @@
-mult_8bit	mult_8bit_inst (
-	.clock ( clock_sig ),
-	.dataa ( dataa_sig ),
-	.datab ( datab_sig ),
-	.result ( result_sig )
-	);
