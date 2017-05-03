4002f13a5883521f1e2c34dcb7177d635505ede7
Alan Ehret <alan.4186@yahoo.com>
2017-05-03 02:00:17 -0400

    add simple add and mult module TBs to auto tb script

--- Simulation/run_all_tesbenches.sh
+++ Simulation/run_all_tesbenches.sh
@@ -11,7 +11,7 @@ $VSIM -batch -do load_files.do
 TESTBENCHES=(adder_tree_32bit_tb mult_adder_tb ram_sr_tb\
   parallel_out_sr_tb layer_sr_tb dense_sr_tb convolution_2D_tb\
   convolution_25D_tb dense_2D_tb dense_25D_tb relu_tb bias_tb\
-  requantize_tb)
+  requantize_tb mult_8bit_signed_tb add_8bit_signed_tb add_32bit_signed_tb)
 
 # inlude any extra libraries needed in the braces after the -L, seperate with commas
 for i in ${TESTBENCHES[@]}; do
