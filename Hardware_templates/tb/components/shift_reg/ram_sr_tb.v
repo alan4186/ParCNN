9450b24871d1d28fc1f7fa9ebfd60eb86c8573de
Alan Ehret <alan.4186@yahoo.com>
2017-05-11 16:18:26 -0400

    Add enable and row_shift to ram_sr

--- Hardware_templates/tb/components/shift_reg/ram_sr_tb.v
+++ Hardware_templates/tb/components/shift_reg/ram_sr_tb.v
@@ -1,32 +1,49 @@
 `timescale 1 ps / 1 ps
 module ram_sr_tb();
+parameter RAM_SR_DEPTH = 10;
+parameter ROW_SHIFT = 3;
 
 reg clock;
 reg reset;
 
-reg [7:0] shift_in;
+reg enable;
+reg shift_row_up;
+reg [7:0] column_shift_in;
 
-wire [7:0] shift_out;
+wire [8*ROW_SHIFT-1:0] row_shift_in;
+
+wire [7:0] column_shift_out;
+wire [8*ROW_SHIFT-1:0] row_shift_out;
 
 // DUT
 ram_sr #(
-  .DEPTH(3)
+  .RAM_SR_DEPTH(RAM_SR_DEPTH),
+  .ROW_SHIFT(ROW_SHIFT)
 )
 dut(
   .clock(clock),
   .reset(reset),
-  .shift_in(shift_in),
-  .shift_out(shift_out)
+  .enable(enable),
+  .shift_row_up(shift_row_up),
+  .column_shift_in(column_shift_in),
+  .row_shift_in(row_shift_in),
+  .column_shift_out(column_shift_out),
+  .row_shift_out(row_shift_out)
 );
 
 
 // shift_in counter
 always@(posedge clock or negedge reset) begin
-  if(reset == 1'b0) 
-    shift_in <= 8'd0;
+  if(reset == 1'b0)
+    column_shift_in <= 8'd0;
   else
-    shift_in <= shift_in + 8'd1;
+    column_shift_in <= column_shift_in + 8'd1;
 end
+
+assign row_shift_in[7:0] = column_shift_in;
+assign row_shift_in[15:8] = column_shift_in + 1;
+assign row_shift_in[23:16] = column_shift_in + 2;
+
 
 always begin
   #5 clock <= ~clock;
@@ -38,14 +55,40 @@ initial begin
   $display("#############");
   clock = 1'b1;
   reset = 1'b1;
-  
+
+  enable = 1'b1;
+  shift_row_up = 1'b0;
+
   #10 reset = 1'b0;
   #10 reset = 1'b1;
 
-  #30 // check output
+  #100 // check output
   $display("Time = %0d",$time);
-  $display("shift_out = %h", shift_out);
-  if( shift_out ==  8'd0) begin
+  $display("column_shift_out = %h", column_shift_out);
+  if( column_shift_out ==  8'd0) begin
+    $display("\t\t\tPASS!");
+  end else begin
+    $display("\t\t\tFAIL!");
+  end // end if/else
+
+  // Shift row
+  shift_row_up = 1'b1;
+  #10
+  $display("Time = %0d",$time);
+  $display("row_shift_out = %h", row_shift_out);
+  if( row_shift_out ==  24'h030405) begin
+    $display("\t\t\tPASS!");
+  end else begin
+    $display("\t\t\tFAIL!");
+  end // end if/else
+
+  shift_row_up = 1'b0;
+  enable = 1'b0;
+  #10
+  enable =1'b1;
+  $display("Time = %0d",$time);
+  $display("row_shift_out = %h", row_shift_out);
+  if( row_shift_out ==  24'h030405) begin
     $display("\t\t\tPASS!");
   end else begin
     $display("\t\t\tFAIL!");
@@ -53,24 +96,14 @@ initial begin
 
   #10
   $display("Time = %0d",$time);
-  $display("shift_out = %h", shift_out);
-  if( shift_out ==  8'd1) begin
+  $display("row_shift_out = %h", row_shift_out);
+  if( row_shift_out ==  24'h040506) begin
     $display("\t\t\tPASS!");
   end else begin
     $display("\t\t\tFAIL!");
   end // end if/else
 
   #10
-  $display("Time = %0d",$time);
-  $display("shift_out = %h", shift_out);
-  if( shift_out ==  8'd2) begin
-    $display("\t\t\tPASS!");
-  end else begin
-    $display("\t\t\tFAIL!");
-  end // end if/else
-
-
-  #100
   $display("\n");
   $stop;
 end
--- Hardware_templates/v/components/shift_reg/ram_sr.v
+++ Hardware_templates/v/components/shift_reg/ram_sr.v
@@ -1,28 +1,65 @@
-// THIS IS A STANDIN FOR THE RAM SR MEGAFUCNTION
 module ram_sr #(
-  parameter DEPTH = -1
+  parameter RAM_SR_DEPTH = -1,
+  parameter ROW_SHIFT = -1
 )(
   input clock,
   input reset,
-  input [7:0] shift_in,
-  output [7:0] shift_out
+  input enable,
+  input shift_row_up,
+  input [7:0] column_shift_in,
+  input [8*ROW_SHIFT-1:0] row_shift_in,
+  output [7:0] column_shift_out,
+  output [8*ROW_SHIFT-1:0] row_shift_out
 );
 
-reg [7:0] sr [DEPTH-1:0];
+wire [7:0] column_shift_in_wire [ROW_SHIFT-1:0];
 
-assign shift_out = sr[DEPTH-1];
+reg [7:0] sr [RAM_SR_DEPTH-1:0];
 
-always@(posedge clock) begin
-  sr[0] <= shift_in;
-end
+assign column_shift_out = sr[RAM_SR_DEPTH-1];
+assign column_shift_in_wire[0] = column_shift_in;
 
 genvar i;
 generate
-for(i=0; i<DEPTH-1; i=i+1) begin : sr_loop
+for(i=1; i<ROW_SHIFT; i=i+1) begin : column_wire_loop
+  // assign values to the colum_in wire to match the
+  // row_shift_in wire length
+  assign column_shift_in_wire[i] = sr[i-1];
+end // for
+
+for(i=0; i<ROW_SHIFT; i=i+1) begin : shift_in_loop
   always@(posedge clock) begin
-    sr[i+1] <= sr[i];
-  end
-end
+    if (enable) begin
+      if(shift_row_up) begin
+        sr[i] <= row_shift_in[8*i+7:8*i];
+      end else begin
+        sr[i] <= column_shift_in_wire[i];
+      end // shift_row_up
+    end else begin
+      sr[i] <= sr[i];
+    end // enable if/else
+  end // always
+end // for
+
+for(i=ROW_SHIFT; i<RAM_SR_DEPTH; i=i+1) begin : sr_loop
+  always@(posedge clock) begin
+    if (enable) begin
+      if(shift_row_up) begin
+        sr[i] <= sr[i-ROW_SHIFT];
+      end else begin
+        sr[i] <= sr[i-1];
+      end // shift row up
+    end else begin
+      sr[i] <= sr[i];
+    end // enable if/else
+  end // always
+end // for
+
+// assign row_shift_out
+for(i=0; i<ROW_SHIFT; i=i+1) begin : assign_row_shift_out
+  assign row_shift_out[8*i+7:8*i] = sr[RAM_SR_DEPTH-ROW_SHIFT+i];
+end // for
+
 endgenerate
 
 endmodule
